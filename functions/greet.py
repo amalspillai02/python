2d1da1c0455e185ec709d4c7abb8e48effad98c4
amal-stacqube <amal@stacqube.com>
2025-08-05 20:24:09 +0530

    Add greet function examples with different parameter usages

---
+++ functions/greet.py
@@ -0,0 +1,25 @@
+# normal function
+'''def greet():
+    print("hii")
+
+
+greet()'''
+
+# function with parameter and argument
+# fname and lname are parameter
+# amal and pillai are argument
+'''def greet(fname, lname):  
+    print(f"hi {fname} {lname}")
+
+
+greet("amal", "pillai")'''
+
+# function taht will return a value
+
+
+'''def greet(name):
+    return (f"{name}")
+
+
+message = greet("amal")
+print(message)'''
